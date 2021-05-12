a3a3a989a2e842acfae92116b79bfd1fb1846c66
Szczepan <s.szablej1@gmail.com>
2021-05-13 00:46:17 +0200

    Provide api controller to the aplication

--- FoodieApp/Startup.cs
+++ FoodieApp/Startup.cs
@@ -32,6 +32,7 @@ namespace FoodieApp
             });
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +59,7 @@ namespace FoodieApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
